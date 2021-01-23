f4904a2e4b3e4410e66df5c645339256504d8f28
nikita.kochetkov <nikita.kochetkov@jetbrains.com>
2021-01-24 01:05:15 +0300

    add register and login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./Login.css"
 
 class Login extends React.Component {
     constructor(props) {
@@ -7,8 +8,20 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                hello login
+            <div className="forma">
+                <form action="#" method="post">
+                    <label htmlFor="email" className="form-element">
+                        <div>Login</div>
+                        <input type="email" id="email" pattern=".+@.+..+" size="30" placeholder="niceDeliver@yandex.ru" required/>
+                    </label>
+                    <label htmlFor="password" className="form-element">
+                        <div>Password</div>
+                        <input type="password" id="password" required/>
+                    </label>
+                    <div className="submit-style">
+                        <input type="submit" value="Login"/>
+                    </div>
+                </form>
             </div>
         );
     }
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./Register.css"
 
 class Register extends React.Component {
     constructor(props) {
@@ -7,8 +8,24 @@ class Register extends React.Component {
 
     render() {
         return (
-            <div>
-                hello register
+            <div className="formaR">
+                <form action="#" method="post">
+                    <label htmlFor="email" className="form-element">
+                        <div>Login or email</div>
+                        <input type="email" id="email" pattern=".+@.+..+" size="30" placeholder="niceDeliver@yandex.ru" required/>
+                    </label>
+                    <label htmlFor="password" className="form-element">
+                        <div>Password</div>
+                        <input type="password" id="password" required/>
+                    </label>
+                    <label htmlFor="bday" className="form-element">
+                        <div>Birthday</div>
+                        <input type="date" name="bday" id="bday"/>
+                    </label>
+                    <div className="submit-style">
+                        <input type="submit" value="Register"/>
+                    </div>
+                </form>
             </div>
         );
     }
